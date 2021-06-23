0ee735f41f70734458e9b1a6c27f15c99b97a07d
Shakhriyor Gafurov <smoke7772@gmail.com>
2021-06-23 17:22:53 +0500

    Removed object file

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -25,6 +25,14 @@ class MainActivity : AppCompatActivity() {
             adapter = testAdapter
         }
 
-        testAdapter.setData(MockData.mockList)
+        testAdapter.setData(mockList)
     }
+
+    val mockList = listOf(
+        ListModel("Ashot", 33),
+        ListModel("Andrey", 23),
+        ListModel("Matvey", 21),
+        ListModel("Vasya", 65),
+        ListModel("Anton", 82)
+    )
 }
